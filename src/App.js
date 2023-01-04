a1c3c9a811a993971a610ed65211d4014399f11a
thomasstrassmann <thomas_strassmann@freenet.de>
2023-01-04 14:55:43 +0000

    install and implement bootstrap and google fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1>Inital commit</h1>
+      <Button variant="success">Success</Button>
     </div>
   );
 }
