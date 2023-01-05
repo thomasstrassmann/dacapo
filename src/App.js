609285e16f145e251bb2d825a5f05667728bb011
thomasstrassmann <thomas_strassmann@freenet.de>
2023-01-05 14:11:17 +0000

    implement routing in header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import NavBar from "./components/NavBar";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
+import Container from "react-bootstrap/Container";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>}/>
+          <Route exact path="/login" render={() => <h1>Login</h1>}/>
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>}/>
+          <Route render={() => <h1>Page does not exist!</h1>}/>
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,21 +4,32 @@ import logo from "../assets/dacapo-logo.png";
 import login from "../assets/icons/login.svg";
 import signup from "../assets/icons/signup.svg";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} fixed="top" expand="md">
       <Container>
+        <NavLink to="/" exact>
         <Navbar.Brand>
           <img src={logo} alt="DaCapo Logo" height="55" />
         </Navbar.Brand>
+        </NavLink>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
         <Navbar.Collapse id="basic-navbar-nav">
+
           <Nav className="ml-auto">
-            <Nav.Link><img src={login} alt="login" height="30"/>Login</Nav.Link>
-            <Nav.Link><img src={signup} alt="signup" height="30"/>Sign up</Nav.Link>
+            <NavLink to="/login" className={styles.NavLink} activeClassName={styles.Active}>
+              <img src={login} alt="login" height="30"/>Login
+              </NavLink>
+            <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
+              <img src={signup} alt="signup" height="30"/>Sign up
+              </NavLink>
           </Nav>
         </Navbar.Collapse>
+
       </Container>
     </Navbar>
   );
