b03ad0cfc287033c141f53ad5b3335cac96cf25b
thomasstrassmann <thomas_strassmann@freenet.de>
2023-01-05 10:55:14 +0000

    add first styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
 import NavBar from "./components/NavBar";
+import styles from "./App.module.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,18 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 import logo from "../assets/dacapo-logo.png";
 import login from "../assets/icons/login.svg";
 import signup from "../assets/icons/signup.svg";
-
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar fixed="top" expand="md">
+    <Navbar className={styles.NavBar} fixed="top" expand="md">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="DaCapo Logo" height="55" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
+          <Nav className="ml-auto">
             <Nav.Link><img src={login} alt="login" height="30"/>Login</Nav.Link>
             <Nav.Link><img src={signup} alt="signup" height="30"/>Sign up</Nav.Link>
           </Nav>
