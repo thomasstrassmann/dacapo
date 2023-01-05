825acfd26c15921bbbe51fd9bf29cf7a94ac47a2
thomasstrassmann <thomas_strassmann@freenet.de>
2023-01-05 08:32:11 +0000

    implement navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Button from 'react-bootstrap/Button';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Button variant="success">Success</Button>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import logo from "../assets/dacapo-logo.png";
+import login from "../assets/icons/login.svg";
+import signup from "../assets/icons/signup.svg";
+
+
+const NavBar = () => {
+  return (
+    <Navbar fixed="top" expand="md">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="DaCapo Logo" height="55" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link><img src={login} alt="login" height="30"/>Login</Nav.Link>
+            <Nav.Link><img src={signup} alt="signup" height="30"/>Sign up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
