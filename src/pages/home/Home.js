56e2ce26103e758195255c429dbc15a0e1c63bbb
thomasstrassmann <thomas_strassmann@freenet.de>
2023-01-13 15:41:05 +0000

    add styles to carousel

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,22 +6,22 @@ import carousel1 from "../../assets/carousel1.jpg";
 import carousel2 from "../../assets/carousel2.jpg";
 import carousel3 from "../../assets/carousel3.jpg";
 
-import style from "../../styles/Home.module.css"
+import styles from "../../styles/Home.module.css"
 
 function Home() {
   const user = useUser();
 
   return (
     <>
-      <Carousel className={style.Carousel}>
+      <Carousel>
         <Carousel.Item interval={5000}>
           <img
-            className={style.Carousel}
+            className={styles.Carousel}
             src={carousel1}
             alt="Let B-Stock Instruments resound again!"
           />
-          <Carousel.Caption>
-            {user && <h1>Welcome {user.username}!</h1>}
+          <Carousel.Caption className={styles.Caption}>
+            {user && <h1>Welcome <span className={styles.Username}>{user.username}!</span></h1>}
             <h2>Let B-Stock Instruments resound again!</h2>
             <p>Sell or buy instruments conveniently.</p>
             <p>No matter if guitar, bass, piano or tuba!</p>
@@ -29,11 +29,11 @@ function Home() {
         </Carousel.Item>
         <Carousel.Item interval={5000}>
           <img
-            className={style.Carousel}
+            className={styles.Carousel}
             src={carousel2}
             alt="Discover your new favorite instrument!"
           />
-          <Carousel.Caption>
+          <Carousel.Caption className={styles.Caption}>
             <h2>Discover your new favorite instrument!</h2>
             <p>
               Browse hundreds of B-Stock instruments at an unbeatable
@@ -42,8 +42,8 @@ function Home() {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={5000}>
-          <img className={style.Carousel} src={carousel3} alt="Third slide" />
-          <Carousel.Caption>
+          <img className={styles.Carousel} src={carousel3} alt="Contact buyers or post an instrument request!" />
+          <Carousel.Caption className={styles.Caption}>
             <h2>Contact buyers or post an instrument request!</h2>
             <p>
               Use every opportunity to get closer to your dream instrument, even
