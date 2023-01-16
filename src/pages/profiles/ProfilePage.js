8451b855468f07a85c10eed9f1d2150d6e062f0f
thomasstrassmann <thomas_strassmann@freenet.de>
2023-01-16 16:43:01 +0000

    implement rating form on profilepage with first logic

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
+import { Alert } from "react-bootstrap";
 
 import Asset from "../../components/Asset";
 import Instrument from "../instruments/Instrument";
@@ -18,19 +20,24 @@ import { ProfileEditDropdown } from "../../components/EditDropdown";
 import { useUser } from "../../contexts/UserContext";
 import { useProfile, useSetProfile } from "../../contexts/ProfileContext";
 import { useParams } from "react-router";
-
 import { axiosReq } from "../../api/axiosDefaults";
 
 import { Button, Image } from "react-bootstrap";
 import { fetchMore } from "../../utils/utils";
 
 import InfiniteScroll from "react-infinite-scroll-component";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const [profileInstruments, setProfileInstruments] = useState({ results: [] });
+  const [errors, setErrors] = useState({});
+  const [ratingData, setRatingData] = useState({ rating: ""});
+  const { rating } = ratingData;
+
   const user = useUser();
   const { id } = useParams();
+  const history = useHistory();
 
   const { pageProfile } = useProfile();
   const { handleFollow, handleUnfollow, setProfile } = useSetProfile();
@@ -58,53 +65,125 @@ function ProfilePage() {
     fetchData();
   }, [id, setProfile]);
 
+  const handleChange = (event) => {
+    setRatingData({
+      ...ratingData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append("profile_id", id);
+    formData.append("rating", ratingData.rating);
+
+    try {
+      await axiosReq.post('/rating/', formData);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
+    }
+  };
+
+  const ratingField = (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Rating</Form.Label>
+          <Form.Control
+            as="select"
+            name="rating"
+            value={rating}
+            onChange={handleChange}
+            placeholder="Rate the seller"
+            aria-label="Rate the seller"
+          >
+            <option value="1">1 Star</option>
+            <option value="2">2 Stars</option>
+            <option value="3">3 Stars</option>
+            <option value="4">4 Stars</option>
+            <option value="5">5 Stars</option>
+          </Form.Control>
+        </Form.Group>
+        {errors?.rating?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+        <Button className={btnStyles.CreateFormButton} type="submit">
+          rate
+        </Button>
+      </Form>
+    </>
+  );
+
   const mainProfile = (
     <>
       {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
       <Row noGutters className="px-3 text-center">
-        <Col lg={3} className="text-lg-left">
+        <Col className="text-lg-left">
           <Image
             className={styles.ProfileImage}
             roundedCircle
             src={profile?.avatar}
           />
         </Col>
-        <Col lg={6}>
+        <Col>
           <h3 className="m-2">{profile?.owner}</h3>
           <Row className="justify-content-center no-gutters">
-            <Col xs={3} className="my-2">
-              <div>{profile?.instruments_count}</div>
-              <div>instruments</div>
+            <Col className="my-2">
+              <div>{profile?.followers_count} followers</div>
             </Col>
-            <Col xs={3} className="my-2">
-              <div>{profile?.followers_count}</div>
-              <div>followers</div>
+            <Col className="my-2">
+              <div>{profile?.following_count} following</div>
             </Col>
-            <Col xs={3} className="my-2">
-              <div>{profile?.following_count}</div>
-              <div>following</div>
+          </Row>
+          <Row>
+            <Col className="my-2">
+              <div>{profile?.instruments_count} instruments</div>
+            </Col>
+            <Col className="my-2">
+              <div>
+                Rating:{" "}
+                {profile?.average_rating === 0
+                  ? "No ratings yet!"
+                  : profile?.average_rating}
+              </div>
             </Col>
           </Row>
         </Col>
-        <Col lg={3} className="text-lg-right">
-          {user &&
-            !is_owner &&
-            (profile?.following_id ? (
-              <Button
-                className={btnStyles.Button}
-                onClick={() => handleUnfollow(profile)}
-              >
-                unfollow
-              </Button>
-            ) : (
-              <Button
-                className={btnStyles.Button}
-                onClick={() => handleFollow(profile)}
-              >
-                follow
-              </Button>
-            ))}
-        </Col>
+        <Row>
+          <Col>
+            {user &&
+              !is_owner &&
+              (profile?.following_id ? (
+                <Button
+                  className={btnStyles.Button}
+                  onClick={() => handleUnfollow(profile)}
+                >
+                  unfollow
+                </Button>
+              ) : (
+                <Button
+                  className={btnStyles.Button}
+                  onClick={() => handleFollow(profile)}
+                >
+                  follow
+                </Button>
+              ))}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {user?.username === profile?.owner
+              ? "You can not rate your own account!"
+              : ratingField}
+          </Col>
+        </Row>
       </Row>
     </>
   );
@@ -139,7 +218,7 @@ function ProfilePage() {
 
   return (
     <Row>
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
+      <Col className="py-2 p-0 p-lg-2">
         <ProfilesOverview mobile />
         <Container className={appStyles.Content}>
           {hasLoaded ? (
@@ -151,9 +230,6 @@ function ProfilePage() {
             <Asset spinner />
           )}
         </Container>
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        <ProfilesOverview />
       </Col>
     </Row>
   );
