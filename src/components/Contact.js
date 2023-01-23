b00b4b2d8a9854822f7e8518b999aa13d75acef4
thomasstrassmann <thomas_strassmann@freenet.de>
2023-01-23 15:46:36 +0000

    implement user feedback after message sent

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 import Form from "react-bootstrap/Form";
 import { useParams } from "react-router-dom/cjs/react-router-dom";
 import styles from "../styles/Contact.module.css";
@@ -7,28 +8,32 @@ import btnStyles from "../styles/Button.module.css";
 import axios from "axios";
 import { useUser } from "../contexts/UserContext";
 
-const Contact = ({query}) => {
+const Contact = ({ query }) => {
   const [contactData, setContactData] = useState({
     text: "",
   });
   const { text } = contactData;
   const { id } = useParams();
-
+  const [show, setShow] = useState(false);
   const [item, setItem] = useState({});
   const [itemOwnerMail, setItemOwnerMail] = useState("");
   const user = useUser();
 
   useEffect(() => {
     const handleMount = async () => {
-        axios.get(`https://dacapo-api.herokuapp.com/${query}/${id}`)
-        .then(response => {
+      axios
+        .get(`https://dacapo-api.herokuapp.com/${query}/${id}`)
+        .then((response) => {
           setItem(response.data);
-          return axios.get(`https://dacapo-api.herokuapp.com/profiles/${response.data.profile_id}`)
+          return axios.get(
+            `https://dacapo-api.herokuapp.com/profiles/${response.data.profile_id}`
+          );
         })
-        .then (response => {
+        .then((response) => {
           setItemOwnerMail(response.data.email);
-        }).catch(error => console.log(error.response));
-    }
+        })
+        .catch((error) => console.log(error.response));
+    };
     handleMount();
   }, [id, itemOwnerMail, query]);
 
@@ -40,15 +45,14 @@ const Contact = ({query}) => {
   };
 
   const sendMail = () => {
-      window.Email.send({
-        SecureToken : "198f07c9-78f3-4213-bc2c-0c7ab34ed62d",
-        To: `${itemOwnerMail}`,
-        From: "dacapo.service@gmail.com",
-        Subject: `Interest in ${item.title}`,
-        Body: `Message from ${user.username} (${user.email}): \n ${text}`,
-      }).then((message) => alert(message));
-    //   implement popup here
-    };
+    window.Email.send({
+      SecureToken: "198f07c9-78f3-4213-bc2c-0c7ab34ed62d",
+      To: `${itemOwnerMail}`,
+      From: "dacapo.service@gmail.com",
+      Subject: `Interest in ${item.title}`,
+      Body: `Message from ${user.username} - (${user.email}): \n ${text}`,
+    }).then(() => setShow(true));
+  };
 
   return (
     <>
@@ -65,10 +69,18 @@ const Contact = ({query}) => {
             required
           />
         </Form.Group>
-        <Button className={btnStyles.ContactButton} onClick={sendMail}>Send message</Button>
+        {show && (
+          <Alert variant="success" onClose={() => setShow(false)} dismissible>
+            <Alert.Heading>Message sent!</Alert.Heading>
+            <p>The seller got your message and will reach out to you!</p>
+          </Alert>
+        )}
+        <Button className={btnStyles.ContactButton} onClick={sendMail}>
+          Send message
+        </Button>
       </Form>
     </>
-  )
+  );
 };
 
 export default Contact;
